7c952c67e3166b7bc3c37d4559876e79417c240f
ShogoFujii <sf_1105@hotmail.com>
2014-11-08 23:43:11 +0900

    commit

---
+++ example/test.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+
+<body>
+
+<?php
+
+print "Hello World!";
+
+//$json = file_get_contents('https://api.tokyometroapp.jp/api/v2/datapoints/urn:ucode:_00001C000000000000010000030C4726?acl:consumerKey=abe9dd11294378e7331f3d33a15487c724ec72848ab35446d80df58d58287e3c');
+
+$url="http://www.yahoo.co.jp/";
+
+$json = file_get_contents($url);
+
+//$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+
+print_r($json);
+
+
+?>
+
+</body>
+</html>
